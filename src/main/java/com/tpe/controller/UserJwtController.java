72d862548eabcff6bf33df76f85cb96de955f160
Samet Genç <114944484+Sametgncc@users.noreply.github.com>
2024-07-01 20:23:11 +0300

    new cart

--- src/main/java/com/tpe/controller/UserJwtController.java
+++ src/main/java/com/tpe/controller/UserJwtController.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.method.P;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
